77fd36cc4afbb6bf4647f9b37bf8cd92c64a8824
Gra-V-Robotics <comcelion@gmail.com>
2017-01-26 20:58:31 -0500

    Added PID Loop Control Driver Station Dashboard Objects

--- 2017TestBed Bot/src/org/usfirst/frc/team5816/robot/Robot.java
+++ 2017TestBed Bot/src/org/usfirst/frc/team5816/robot/Robot.java
@@ -6,9 +6,11 @@ import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
 import com.ctre.CANTalon;
+import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.GyroBase;
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -16,13 +18,16 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.PIDCommand;
 import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
@@ -38,7 +43,7 @@ public class Robot extends IterativeRobot {
 	
 	Joystick logitechController = new Joystick(0);
 	
-	NetworkTable table;
+	//AHRS navX = new AHRS(SerialPort.Port.kMXP);
 	
 	CANTalon frontLeftMotor = new CANTalon(4);
 	CANTalon frontRightMotor = new CANTalon(2);
@@ -51,41 +56,85 @@ public class Robot extends IterativeRobot {
 	double rightEncoderVal;
 	
 	RobotDrive driveTrain = new RobotDrive(frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor);
+	
+	PIDController anglePIDController;
+	PIDSource anglePIDSource;
+	PIDOutput anglePIDOutput;
+	
+	Double angleKp;
+	Double angleKi;
+	Double angleKd;
+	
+	Double angleOutput;
+	
+	PIDController encoderPIDController;
+	PIDSource encoderPIDSource;
+	PIDOutput encoderPIDOutput;
+	
+	Double encoderKp;
+	Double encoderKi;
+	Double encoderKd;
+	
+	Double encoderOutput;
+	
+	DriverStation driverStation;
+	
+	String driverStationAlliance;
+	
+	Double driverStationLocation;
+	Double batteryVoltage;
+	Double matchTime;
 
 	@Override
 	public void robotInit() {
 		sensorCalibration();
-		//this.table = NetworkTable.getTable("ShooterCamera");
+		gyroRobotInit();
 	}
 
 	@Override
 	public void autonomousInit() {
-		resetSensors();
+		resetSensors();	
+		gyroAutoInit();
 	}
 
 	@Override
 	public void autonomousPeriodic() {
+		updateSensors();
+		gyroAutoPeriodic();
 
+		SmartDashboard.putData("AnglePID", this.anglePIDController);
+		SmartDashboard.putNumber("GyroAngleVal: ", gyroAngleVal);
+		SmartDashboard.putNumber("GyroRateVal: ", gyroRateVal);
+		SmartDashboard.putNumber("LeftEncoderVal: ", leftEncoderVal);
+		SmartDashboard.putNumber("RightEncoderVal: ", -rightEncoderVal);
+		
 	}
 	
 	@Override
 	public void teleopInit() {
 		resetSensors();
-		
 	}
 
 	@Override
 	public void teleopPeriodic() {
-		dynamicVariableInitilization();
+
+		updateSensors();
 		driveWithJoysticks(logitechController.getRawAxis(1), logitechController.getRawAxis(5));
 		SmartDashboard.putNumber("GyroAngleVal: ", gyroAngleVal);
 		SmartDashboard.putNumber("GyroRateVal: ", gyroRateVal);
 		SmartDashboard.putNumber("LeftEncoderVal: ", leftEncoderVal);
 		SmartDashboard.putNumber("RightEncoderVal: ", -rightEncoderVal);
+		SmartDashboard.putString("Driver Station Aliance: ", driverStationAlliance);
+		SmartDashboard.putNumber("Driver Station Location: ", driverStationLocation);
+		SmartDashboard.putNumber("Battery Voltage: ", batteryVoltage);
+		SmartDashboard.putNumber("Match Time: ", matchTime);
 	}
 
 	@Override
 	public void testPeriodic() {
+		
+		driveWithJoysticks(logitechController.getRawAxis(1), logitechController.getRawAxis(5));
+		SmartDashboard.putNumber("LeftEncoderRate", leftEncoder.getRate());
 		
 	}
 	
@@ -104,11 +153,102 @@ public class Robot extends IterativeRobot {
 		driveTrain.tankDrive(-leftVal, -rightVal);
 	}
 	
-	public void dynamicVariableInitilization() {
+	public void updateSensors() {
 		gyroAngleVal = gyro.getAngle();
 		gyroRateVal = gyro.getRate();
 		leftEncoderVal = leftEncoder.get();
 		rightEncoderVal = rightEncoder.get();
+	}
+	
+	public void gyroRobotInit() {
+		
+		this.angleKd = 0.12d;
+		this.angleKi = 0.0001d;
+		this.angleKp = 0.16d;
+		this.anglePIDSource = new PIDSource() {
+		
+		@Override
+		public void setPIDSourceType(PIDSourceType pidSource) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		@Override
+		public double pidGet() {
+			Double angle = gyro.getAngle();
+			return angle==null ? 0 : angle;
+		}
+		
+		@Override
+		public PIDSourceType getPIDSourceType() {
+			
+			return PIDSourceType.kDisplacement;
+		}
+	};
+	this.anglePIDOutput = new PIDOutput() {
+		
+		@Override
+		public void pidWrite(double output) {
+			angleOutput = output;
+			
+		}
+	};
+	this.anglePIDController = new PIDController(angleKp, angleKi, angleKd, anglePIDSource, anglePIDOutput);
+
+	}
+	
+	public void gyroAutoInit() {
+		this.anglePIDController.setSetpoint(25);
+		this.anglePIDController.setContinuous();
+		this.anglePIDController.enable();
+	}
+	
+	public void gyroAutoPeriodic() {
+		
+		System.out.println(this.angleOutput);
+		try{
+			frontLeftMotor.set(this.angleOutput * 0.5);
+			backLeftMotor.set(this.angleOutput * 0.5);
+			frontRightMotor.set(this.angleOutput * 0.5);
+			backRightMotor.set(this.angleOutput * 0.5);
+		}catch(Exception e){
+			
+		}
+	}
+	
+	public void encoderRobotInit() {
+		encoderPIDSource = new PIDSource() {
+
+			@Override
+			public void setPIDSourceType(PIDSourceType pidSource) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public PIDSourceType getPIDSourceType() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public double pidGet() {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+			
+		};
+	}
+	
+	public void encoderAutonomousInit() {
+		
+	}
+	
+	public void driverStationDisplay() {
+		driverStationAlliance = driverStation.getAlliance().toString();
+		driverStationLocation = (double) driverStation.getLocation();
+		batteryVoltage = driverStation.getBatteryVoltage();
+		matchTime = driverStation.getMatchTime();
 	}
 
 }
